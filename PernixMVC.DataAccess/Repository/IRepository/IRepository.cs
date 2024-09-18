972d21187f413dd4a86e08a9700e318507f11ffe
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-09-18 17:07:25 +0930

    Added Home UI and Bootsrap

--- PernixMVC.DataAccess/Repository/IRepository/IRepository.cs
+++ PernixMVC.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,8 +10,8 @@ namespace PernixMVC.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         //T - Category
-        IEnumerable<T> GetAll(string? includesProperties = null);
-        T Get(Expression<Func<T, bool>> filter, string? includesProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
--- PernixMVC.DataAccess/Repository/Repository.cs
+++ PernixMVC.DataAccess/Repository/Repository.cs
@@ -29,9 +29,19 @@ namespace PernixMVC.DataAccess.Repository
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet;
+
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
+
             query = query.Where(filter);
             if (!string.IsNullOrEmpty(includeProperties))
             {
@@ -45,13 +55,17 @@ namespace PernixMVC.DataAccess.Repository
 
         }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
             if (!string.IsNullOrEmpty(includeProperties))
             {
-                foreach(var includeProp in includeProperties
-                    .Split(new char[] { ','},StringSplitOptions.RemoveEmptyEntries))
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
                     query = query.Include(includeProp);
                 }
