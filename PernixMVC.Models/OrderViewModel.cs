ad21f36729c9418aea89374a79d9c4d87a9bb334
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-09-24 16:51:33 +0930

    Adding Order Managment to application

---
+++ PernixMVC.Models/OrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PernixMVC.Models
+{
+    public class OrderViewModel
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> orderDetail { get; set; }
+    }
+}
---
+++ PernixMVC/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PernixMVC.DataAccess.Repository.IRepository;
+using PernixMVC.Models;
+using PernixMVC.Utility;
+using System.Diagnostics;
+namespace PernixMVC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+                {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                    break;
+                case "inprogress":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusInProgress);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusApproved);
+                    break;
+                default:
+                    break;
+
+                }
+            return Json(new { data = objOrderHeaders });
+        }
+
+
+        #endregion
+    }
+}
