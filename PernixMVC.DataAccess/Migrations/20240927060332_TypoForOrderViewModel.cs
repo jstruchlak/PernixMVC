3f5908b40919ca6aeb3b44b632fe0efa1f7b26ab
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-09-27 16:21:57 +0930

    Added ViewComponent Cocept to application

---
+++ PernixMVC.DataAccess/Migrations/20240927060332_TypoForOrderViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PernixMVC.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class TypoForOrderViewModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
